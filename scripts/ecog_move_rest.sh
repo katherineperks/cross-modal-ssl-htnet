1a80ba036804a45c833152b9d5eb1911c4d13cc0
Katherine <kperks@uw.edu>
2022-04-14 17:40:50 -0700

    setup to run xdc on EC01 move vs. rest

--- scripts/ecog_move_rest.sh
+++ scripts/ecog_move_rest.sh
@@ -3,7 +3,7 @@
 # (modified from from https://github.com/yukimasano/self-label)
 device="0"
 DIR='.../ecog_move_rest/ .../ecog_move_rest/pose/'
-subjects='EC01 EC02 EC03 EC04 EC05 EC06 EC07 EC08 EC09 EC10 EC11 EC12'
+subjects='EC01' #EC02 EC03 EC04 EC05 EC06 EC07 EC08 EC09 EC10 EC11 EC12'
 run_num=1
 savepath=.../xdc_runs/naturalistic
 param_lp=../models/naturalistic  # model parameter file to use
@@ -51,61 +51,61 @@ do
 done
 
 
-#########SUPERVISED MODEL#########
+# #########SUPERVISED MODEL#########
 
-IS_SUP="True" # is supervised
-IS_XDC="False" # is cross-modal
-NEP=40 # number of epochs
-nopts=20 # number of times to update pseudo-labels
+# IS_SUP="True" # is supervised
+# IS_XDC="False" # is cross-modal
+# NEP=40 # number of epochs
+# nopts=20 # number of times to update pseudo-labels
 
-for SBJ in $subjects
-do
-    python3 main_n_multimodel_cv.py \
-            --device ${device} \
-            --imagenet-path ${DIR} \
-            --batch-size ${BS} \
-            --augs ${AUG} \
-            --epochs ${NEP} \
-            --nopts ${nopts} \
-            --hc ${HC} \
-            --arch ${ARCH} \
-            --ncl ${NCL} \
-            --workers ${WORKERS} \
-            --pat_id ${SBJ} \
-            --use_ecog ${USE_ECOG} \
-            --n_folds ${NFOLDS} \
-            --is_supervised ${IS_SUP} \
-            --is_xdc ${IS_XDC} \
-            --savepath ${savepath}_supervised_run${run_num}/ \
-            --param_lp ${param_lp};
-done
+# for SBJ in $subjects
+# do
+#     python3 main_n_multimodel_cv.py \
+#             --device ${device} \
+#             --imagenet-path ${DIR} \
+#             --batch-size ${BS} \
+#             --augs ${AUG} \
+#             --epochs ${NEP} \
+#             --nopts ${nopts} \
+#             --hc ${HC} \
+#             --arch ${ARCH} \
+#             --ncl ${NCL} \
+#             --workers ${WORKERS} \
+#             --pat_id ${SBJ} \
+#             --use_ecog ${USE_ECOG} \
+#             --n_folds ${NFOLDS} \
+#             --is_supervised ${IS_SUP} \
+#             --is_xdc ${IS_XDC} \
+#             --savepath ${savepath}_supervised_run${run_num}/ \
+#             --param_lp ${param_lp};
+# done
 
 
-#########UNIMODAL MODEL#########
+# #########UNIMODAL MODEL#########
 
-IS_SUP="False" # is supervised
-IS_XDC="False" # is cross-modal
-NEP=40 # number of epochs
-nopts=20 # number of times to update pseudo-labels
+# IS_SUP="False" # is supervised
+# IS_XDC="False" # is cross-modal
+# NEP=40 # number of epochs
+# nopts=20 # number of times to update pseudo-labels
 
-for SBJ in $subjects
-do
-    python3 main_n_multimodel_cv.py \
-            --device ${device} \
-            --imagenet-path ${DIR} \
-            --batch-size ${BS} \
-            --augs ${AUG} \
-            --epochs ${NEP} \
-            --nopts ${nopts} \
-            --hc ${HC} \
-            --arch ${ARCH} \
-            --ncl ${NCL} \
-            --workers ${WORKERS} \
-            --pat_id ${SBJ} \
-            --use_ecog ${USE_ECOG} \
-            --n_folds ${NFOLDS} \
-            --is_supervised ${IS_SUP} \
-            --is_xdc ${IS_XDC} \
-            --savepath ${savepath}_sep_run${run_num}/ \
-            --param_lp ${param_lp};
-done
+# for SBJ in $subjects
+# do
+#     python3 main_n_multimodel_cv.py \
+#             --device ${device} \
+#             --imagenet-path ${DIR} \
+#             --batch-size ${BS} \
+#             --augs ${AUG} \
+#             --epochs ${NEP} \
+#             --nopts ${nopts} \
+#             --hc ${HC} \
+#             --arch ${ARCH} \
+#             --ncl ${NCL} \
+#             --workers ${WORKERS} \
+#             --pat_id ${SBJ} \
+#             --use_ecog ${USE_ECOG} \
+#             --n_folds ${NFOLDS} \
+#             --is_supervised ${IS_SUP} \
+#             --is_xdc ${IS_XDC} \
+#             --savepath ${savepath}_sep_run${run_num}/ \
+#             --param_lp ${param_lp};
+# done
